734b9e6e76fc96e5bfd50b680d65f3d02823e997
Xtsen <xts.enn@gmail.com>
2021-04-29 16:02:48 +0200

    change link to github repestory

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -1,5 +1,5 @@
 console.log("Un projet mené par Nesta et Louis")
-console.log("le lien vers le github : https://github.com/xtsen/NSI-project")
+console.log("le lien vers le github : https://github.com/xtsen/Skate-in-peace")
 
 var btn = document.getElementById("myBtn");
 
