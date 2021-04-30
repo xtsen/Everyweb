19273f1073bfc5566d9dbb845956409ea9984825
Xtsen <xts.enn@gmail.com>
2021-04-30 23:14:51 +0200

    Create and finish header

---
+++ scripts/scriptAbout.js
@@ -0,0 +1,7 @@
+console.log("Un projet mené par Nesta et Louis")
+console.log("le lien vers le github : https://github.com/xtsen/Skate-in-peace")
+
+function showTitle(idDiv, idEye) {
+    document.getElementById(idDiv).className="hideHeroTitle";
+    document.getElementById(idEye).className="hideHeroTitle";
+}
