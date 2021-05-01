a44e3b7555ec59ee92b629bf8fa631e169a9f19a
Xtsen <xts.enn@gmail.com>
2021-05-01 17:58:28 +0200

    phone size index

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -3,7 +3,7 @@ console.log("le lien vers le github : https://github.com/xtsen/Skate-in-peace")
 
 function load() {
     document.getElementById("body").className = "animationBody";
-    
+
 }
 
 var btn = document.getElementById("myBtn");
@@ -22,9 +22,15 @@ function videoClose(idSpan, idVideo) {
     video.currentTime = 0;
 }
 
-window.onscroll = function() {SideSlideImage()};
-
-function SideSlideImage() {
+if (window.screen.availWidth > 1020) {
+    window.onscroll = function() {SideSlideImageDesktop()};
+    console.log("desktop size")
+}
+else if (window.screen.availWidth > 319) {
+    window.onscroll = function() {SideSlideImagePhone()};
+    console.log("phone size")
+} 
+function SideSlideImageDesktop() {
     if (document.documentElement.scrollTop > 650 && document.documentElement.scrollTop < 2100) {
         document.getElementById("sideSlide").className = "sideSkateDescription";
     }else if (document.body.scrollTop < 650 || document.documentElement.scrollTop < 650) {
@@ -32,4 +38,13 @@ function SideSlideImage() {
     }else if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
         document.getElementById("sideSlide").className = "hiddenSideSlide";
     }
-}
+} 
+function SideSlideImagePhone() {
+    if (document.documentElement.scrollTop > 350 && document.documentElement.scrollTop < 1000) {
+        document.getElementById("sideSlide").className = "sideSkateDescription";
+    }else if (document.body.scrollTop < 200 || document.documentElement.scrollTop < 200) {
+        document.getElementById("sideSlide").className = "hiddenSideSlide";
+    }else if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById("sideSlide").className = "hiddenSideSlide";
+    }
+} 
