c0ca208e18be9c5ef91ee2f99d1df8f19e339443
Xtsen <xts.enn@gmail.com>
2021-05-01 12:30:07 +0200

    remove scroll aniamtion on header

--- scripts/scriptAbout.js
+++ scripts/scriptAbout.js
@@ -8,18 +8,3 @@ function showTitle(idDiv, idEye) {
     document.getElementById(idDiv).className="hideHeroTitle";
     document.getElementById(idEye).className="hideHeroTitle";
 }
-
-window.onscroll = function() {SideSlideImage()};
-
-function SideSlideImage() {
-    if (document.documentElement.scrollTop > 350 && document.documentElement.scrollTop < 400) {
-        document.getElementById("hideHeroTitle2").className = "hideHeroTitle";
-        document.getElementById("eye2").className = "hideHeroTitle";
-    }else if (document.documentElement.scrollTop < 300) {
-        document.getElementById("hideHeroTitle1").className = "hideHeroTitle";
-        document.getElementById("eye1").className = "hideHeroTitle";
-    }else if (document.documentElement.scrollTop > 450) {
-        document.getElementById("hideHeroTitle3").className = "hideHeroTitle";
-        document.getElementById("eye3").className = "hideHeroTitle";
-    }
-}
