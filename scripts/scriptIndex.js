bb04914002ce7d2b495cc8dd7f8e839abcdb48d8
Xtsen <xts.enn@gmail.com>
2021-05-01 02:33:52 +0200

    add page transition

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -1,6 +1,11 @@
 console.log("Un projet mené par Nesta et Louis")
 console.log("le lien vers le github : https://github.com/xtsen/Skate-in-peace")
 
+function load() {
+    document.getElementById("body").className = "animationBody";
+    
+}
+
 var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
