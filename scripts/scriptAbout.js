085ddb5e6fc45bd0b05218ae5b07ada08eb84e21
Xtsen <xts.enn@gmail.com>
2021-05-01 02:57:25 +0200

    add animation aboutPage

--- scripts/scriptAbout.js
+++ scripts/scriptAbout.js
@@ -8,3 +8,18 @@ function showTitle(idDiv, idEye) {
     document.getElementById(idDiv).className="hideHeroTitle";
     document.getElementById(idEye).className="hideHeroTitle";
 }
+
+window.onscroll = function() {SideSlideImage()};
+
+function SideSlideImage() {
+    if (document.documentElement.scrollTop > 300 && document.documentElement.scrollTop < 350) {
+        document.getElementById("hideHeroTitle2").className = "hideHeroTitle";
+        document.getElementById("eye2").className = "hideHeroTitle";
+    }else if (document.documentElement.scrollTop < 200) {
+        document.getElementById("hideHeroTitle1").className = "hideHeroTitle";
+        document.getElementById("eye1").className = "hideHeroTitle";
+    }else if (document.documentElement.scrollTop > 450) {
+        document.getElementById("hideHeroTitle3").className = "hideHeroTitle";
+        document.getElementById("eye3").className = "hideHeroTitle";
+    }
+}
