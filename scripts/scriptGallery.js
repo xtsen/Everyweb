35771f40d00eee57ab2a71f16c44039d94c78791
Xtsen <xts.enn@gmail.com>
2021-05-05 19:06:52 +0200

    algo filter by tricks

--- scripts/scriptGallery.js
+++ scripts/scriptGallery.js
@@ -16,16 +16,17 @@ function videoClose(idSpan, idVideo) {
     video.pause();
     video.currentTime = 0;
 }
-
-// Search function
-function searchName() {
+function search() {
     // Reset
     AllVideos = ["containerVideo1", "containerVideo2", "containerVideo3", "containerVideo4", "containerVideo5", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo12", "containerVideo13", "containerVideo14", "containerVideo15", "containerVideo16", "containerVideo17", "containerVideo18", "containerVideo19", "containerVideo20"]
     AllVideos.forEach(function(container) {
         document.getElementById(container).style.display="flex"
-        console.log(container)
     })
-
+    searchName();
+    searchTricks();
+}
+// Search function
+function searchName() {
     // Recuperation de la valeur du filtre
     choiceName = document.getElementById("name-select").value;
 
@@ -59,7 +60,39 @@ function searchName() {
     else if (choiceName == "Mickael") {
         MickaelVideos.forEach(element => document.getElementById(element).style.display = "none");
     }
-    else {
-        console.log("choix incorect")
+}
+function searchTricks() {
+    // Recuperation de la valeur du filtre
+    choiceTricks= document.getElementById("tricks-select").value;
+
+    // Enumeration des container
+    OllieVideos = ["containerVideo1", "containerVideo2", "containerVideo3", "containerVideo4", "containerVideo5", "containerVideo12", "containerVideo13", "containerVideo15", "containerVideo16", "containerVideo17", "containerVideo18", "containerVideo19", "containerVideo20"]
+    SlideVideos = ["containerVideo1", "containerVideo2", "containerVideo3", "containerVideo4", "containerVideo5", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo14", "containerVideo15", "containerVideo16", "containerVideo17", "containerVideo18", "containerVideo19", "containerVideo20"]
+    BacksideVideos = ["containerVideo2", "containerVideo3", "containerVideo4", "containerVideo5", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo12", "containerVideo13", "containerVideo14", "containerVideo15", "containerVideo18", "containerVideo19", "containerVideo20"]
+    KickflipVideos = ["containerVideo1", "containerVideo2", "containerVideo5", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo12", "containerVideo13", "containerVideo15", "containerVideo16", "containerVideo17", "containerVideo18", "containerVideo19"]
+    ManualVideos = ["containerVideo1", "containerVideo2", "containerVideo3", "containerVideo4", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo12", "containerVideo13", "containerVideo14", "containerVideo15", "containerVideo16", "containerVideo17", "containerVideo18", "containerVideo19", "containerVideo20"]
+    VarialVideos = ["containerVideo1", "containerVideo2", "containerVideo3", "containerVideo4", "containerVideo5", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo12", "containerVideo13", "containerVideo14", "containerVideo16", "containerVideo17", "containerVideo18", "containerVideo19", "containerVideo20"]
+    PopeShovItVideos = ["containerVideo1", "containerVideo2", "containerVideo3", "containerVideo4", "containerVideo5", "containerVideo6", "containerVideo7", "containerVideo8", "containerVideo9", "containerVideo11", "containerVideo12", "containerVideo13", "containerVideo14", "containerVideo15", "containerVideo16", "containerVideo17", "containerVideo19", "containerVideo20"]
+
+    if (choiceTricks == "ollie") {
+        OllieVideos.forEach(element => document.getElementById(element).style.display = "none");
+    }
+    else if (choiceTricks == "slide") {
+        SlideVideos.forEach(element => document.getElementById(element).style.display = "none");
+    }
+    else if (choiceTricks == "backside") {
+        BacksideVideos.forEach(element => document.getElementById(element).style.display = "none");
+    }
+    else if (choiceTricks == "kickflip") {
+        KickflipVideos.forEach(element => document.getElementById(element).style.display = "none");
+    }
+    else if (choiceTricks == "manual") {
+        ManualVideos.forEach(element => document.getElementById(element).style.display = "none");
+    }
+    else if (choiceTricks == "varial") {
+        VarialVideos.forEach(element => document.getElementById(element).style.display = "none");
+    }
+    else if (choiceTricks == "popeShovIt") {
+        PopeShovItVideos.forEach(element => document.getElementById(element).style.display = "none");
     }
 }
