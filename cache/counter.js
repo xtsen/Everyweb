c02afc62f671e1e294b48e231c4b5b555bd38cd9
Xtsen <xts.enn@gmail.com>
2021-05-06 21:06:34 +0200

    add hidden websites

--- cache/counter.js
+++ cache/counter.js
@@ -1,8 +1,8 @@
-function chrono() {
+function chronoNesta() {
     const now = new Date().getTime();
     const countDown = new Date('April 6, 2022').getTime();
     
-    const text = document.getElementById('counter');
+    const text = document.getElementById('counterNesta');
 
     const stopCounter = countDown - now;
 
@@ -14,8 +14,23 @@ function chrono() {
     text.innerText = `${days}j, ${hours}h, ${minutes}m, ${seconds}s`;
 }
 
+function chronoLouis() {
+    const now = new Date().getTime();
+    const countDown = new Date('January 4, 2022').getTime();
+    
+    const text = document.getElementById('counterLouis');
 
+    const stopCounter = countDown - now;
+
+    const days = Math.floor(stopCounter / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((stopCounter % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((stopCounter % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((stopCounter % (1000 * 60)) / 1000)
+
+    text.innerText = `${days}j, ${hours}h, ${minutes}m, ${seconds}s`;
+}
 
 const RefreshInterval = setInterval(() => {
-    chrono()
+    chronoNesta();
+    chronoLouis();
 }, 1000)
