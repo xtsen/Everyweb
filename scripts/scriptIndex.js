bd45a81a2e3798ea8b92426c5cf4fec70edc549e
Xtsen <xts.enn@gmail.com>
2021-05-23 02:02:25 +0200

    create js for index

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -1,7 +1,28 @@
 function redirect(numberWebsite) {
-    websites = ['file:///Users/nesta/Desktop/Developpement/frontend/2021/Skate%20in%20peace/Skate-in-peace/home.html', 
-                'file:///Users/nesta/Desktop/Developpement/frontend/2021/Skate%20in%20peace/Skate%20Escape/skateEscape.html', 
-                'file:///Users/nesta/Desktop/Developpement/frontend/2021/Skate%20in%20peace/Just%20Listen/justListen.html', 
-                'file:///Users/nesta/Desktop/Developpement/frontend/2021/Skate%20in%20peace/Skate-in-peace/cache/2021.html']
+    websites = ['Skate-in-peace/home.html', 
+                'Skate%20Escape/skateEscape.html', 
+                'Just%20Listen/justListen.html', 
+                'Skate-in-peace/cache/2021.html']
     window.location.replace(websites[numberWebsite]);
+}
+
+
+var games = ['website2']
+
+function setView(nameInput) {
+    var checkbox = document.getElementById(nameInput).checked
+
+    if (checkbox) {
+        removeGames();
+    }
+    else if (checkbox != true){
+        viewAll();
+    }
+}
+
+function removeGames() {
+    games.forEach(game => document.getElementById(game).style.display="none")
+}
+function viewAll() {
+    games.forEach(game => document.getElementById(game).style.display="block")
 }
