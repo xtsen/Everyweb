ff7263528277289632c9a90eb98fc75cae60739e
Xtsen <xts.enn@gmail.com>
2021-05-24 11:32:44 +0200

    uncheck checkbox

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -9,6 +9,10 @@ function redirect(numberWebsite) {
 // Liste de card ne correspondant pas au critère
 var games = ['website1', 'website3', 'website4']
 var music = ['website1', 'website2', 'website4']
+var skate = [ 'website3', 'website4']
+
+// Liste de critères 
+var critères = ['game', 'music', 'skate']
 
 function setView(nameInput) {
     var checkbox = document.getElementById(nameInput).checked
@@ -28,10 +32,17 @@ function removeCards(nameInput) {
     else if (nameInput == 'music') {
         music.forEach(card => document.getElementById(card).style.display="none")
     }
+    else if (nameInput == 'skate') {
+        skate.forEach(card => document.getElementById(card).style.display="none")
+    }
     
 }
 function viewAll() {
     // Ajouter à chaque fois les nouvelles listes
     games.forEach(card => document.getElementById(card).style.display="block")
     music.forEach(card => document.getElementById(card).style.display="block")
+    skate.forEach(card => document.getElementById(card).style.display="block")
+
+    // Decocher tous les critères
+    critères.forEach(critère => document.getElementById(critère).checked = false)
 }
