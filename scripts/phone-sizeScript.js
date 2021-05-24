3857582a2d06553f526897e5b01f6c3f26fd49a1
Xtsen <xts.enn@gmail.com>
2021-05-24 14:56:57 +0200

    phone advertising

--- scripts/phone-sizeScript.js
+++ scripts/phone-sizeScript.js
@@ -1,6 +1,6 @@
 function checkWidth() {
     if (window.screen.width <= 1000) {
-        window.location.replace("../Everyweb/index.html")
+        window.location.replace("../Everyweb/phone-Advertising.html")
     }
 
 }
