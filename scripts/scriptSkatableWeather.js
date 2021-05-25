78aa68fbaf6491f6bf51af521afb9a19cbb23163
Xtsen <xts.enn@gmail.com>
2021-05-26 01:58:46 +0200

    js made

--- scripts/scriptSkatableWeather.js
+++ scripts/scriptSkatableWeather.js
@@ -14,10 +14,47 @@ function initDate() {
 }
 initDay()
 initDate()
+let weather = {
+  apiKey: "05102432d68f5cf4fbeab5852f88e77c",
+  fetchWeather: function (city) {
+    fetch( "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey )
+      .then((response) => {
+        if (!response.ok) {
+          alert("Aucune météo trouvé.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
 
-const CLEFAPI = '05102432d68f5cf4fbeab5852f88e77c';
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
 
-let url = 'https://api.openweathermap.org/data/2.5/weather?q=Rouen&appid=05102432d68f5cf4fbeab5852f88e77c';
-fetch(url).then((response) => 
-    response.json().then((data) => console.log(data))
-);
+    document.querySelector(".city").innerText = "Météo à " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidité: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Vitesse du vent : " + speed + " km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.getElementById("inputCity").value);
+  },
+};
+
+document.querySelector(".btnCity").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .getElementById("inputCity")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Aizenay");
