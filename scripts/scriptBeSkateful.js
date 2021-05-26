586472188b5c49a111aa542921bae82290ca8512
Xtsen <xts.enn@gmail.com>
2021-05-26 22:54:38 +0200

    finish default js

--- scripts/scriptBeSkateful.js
+++ scripts/scriptBeSkateful.js
@@ -1,30 +1,38 @@
 const nbOfQuestions = 20;
-const questions = ["Qui suis je ?", "Qui es tu ?"];
-const Answers = ["Nesta", "Louis", "Lucie", "Marie", "Claude", "Marc", "Lourdingue", "Moi"];
+const questions = ["First Question", "Qui suis je ?", "Qui es tu ?", "Lolilol"];
+const Answers = ["Nesta", "Louis", "Lucie", "Marie", "Claude", "Marc", "Lourdingue", "Moi", "Claude", "Marc", "Lourdingue", "Moi"];
+const StartAnswers = [0, 4, 8]
 
 function startQuizz() {
+    var i=0;
     document.getElementById("start").style.display = "none";
 
     document.getElementById("question").style.display ="block";
-
-    q = 1;
-
-    while (q < nbOfQuestions) {
-        NextQuestion(q)
-        q++;
+    titleQuestion = document.querySelector(".titleQuestion").innerText;
+    // console.log(i)
+    // console.log(questions[i], titleQuestion)
+    while (questions[i] != titleQuestion) {
+        i++;
     }
+    actualQuestion = i+1;
+    console.log("Question : " + actualQuestion)
+    NextQuestion(actualQuestion)
     
 }
 function NextQuestion(nbActualQuestion) {
-    console.log("next Question")
 
     document.querySelector(".titleQuestion").innerText = questions[nbActualQuestion];
-
-    i = 0;
-    while (i < i+4) {
+    
+    i = StartAnswers[nbActualQuestion-1]
+    console.log(i)
+    max = i + 4
+    nbLabel = 0
+    console.log(max)
+    while (i < max) {
         i++;
-        id = "labelAnswer" + String(i)
+        nbLabel += 1;
+        id = "labelAnswer" + String(nbLabel)
+        console.log(id)
         document.getElementById(id).innerText = Answers[i-1]
     }
-    i += 4;
 }
