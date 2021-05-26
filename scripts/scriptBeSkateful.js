4ef4c654601e7378d509bdf0abc9f9151d29e325
Xtsen <xts.enn@gmail.com>
2021-05-26 23:23:02 +0200

    add questions and counter

--- scripts/scriptBeSkateful.js
+++ scripts/scriptBeSkateful.js
@@ -1,21 +1,41 @@
-const nbOfQuestions = 20;
-const questions = ["First Question", "Qui suis je ?", "Qui es tu ?", "Lolilol"];
-const Answers = ["Nesta", "Louis", "Lucie", "Marie", "Claude", "Marc", "Lourdingue", "Moi", "Claude", "Marc", "Lourdingue", "Moi"];
-const StartAnswers = [0, 4, 8]
+const questions = ["First Question", 
+                    "Quand apparait le skate ?", 
+                    "Ou commence le skate ?", 
+                    "Combien de skaters en 2008 ?", 
+                    "Quels styles de skate existent ?", 
+                    "À la fin des années 70, quelle figure a révolutionné le skateboard ?", 
+                    "En quel année le jeu Tony Hawk’s Pro Skater est il sorti ?", 
+                    "Qui à fait en premier un 900 ?", 
+                    "Quel est le slogan du fameux magazine Trasher ?"];
 
+const Answers = ["vers 1930", "début 1900", "vers 1950", "vers 1980", 
+                "Nantes", "Paris", "Californie", "New York", 
+                "10 millions", "15 millions", "12 millions", "14 millions",
+                "Cruiser", "Longboard", "Street", "Shortboard",
+                "Le ollie", "Le hardflip", "Le kickflip", "Le 180",
+                "1960", "1970", "1989", "1999", 
+                "Tony Alva", "Eric Koston", "Steve Caballero", "Tony Hawk", 
+                "Skate and Enjoy", "Skate and Destroy", "Skate and Jump", "Skate and Ride"
+            ];
+const StartAnswers = [0, 4, 8, 12, 16, 20]
+
+const nbOfQuestions = questions.length;
+// console.log(questions.length)
 function startQuizz() {
-    var i=0;
+    
     document.getElementById("start").style.display = "none";
-
     document.getElementById("question").style.display ="block";
+
     titleQuestion = document.querySelector(".titleQuestion").innerText;
     // console.log(i)
     // console.log(questions[i], titleQuestion)
+    var i=0;
     while (questions[i] != titleQuestion) {
         i++;
     }
     actualQuestion = i+1;
-    console.log("Question : " + actualQuestion)
+    counter = document.getElementById("counterQuestion")
+    counterQuestion.innerText = "Question : " + actualQuestion + " / " + nbOfQuestions
     NextQuestion(actualQuestion)
     
 }
