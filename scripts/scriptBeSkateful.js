25045e5cb4f61546e899eca809b0198031f01124
Xtsen <xts.enn@gmail.com>
2021-05-26 20:51:49 +0200

    debut script js

--- scripts/scriptBeSkateful.js
+++ scripts/scriptBeSkateful.js
@@ -1,16 +1,30 @@
-nbOfQuestions = 20;
-questions = ["Qui suis je ?", "Qui es tu ?"]
-Answers = ["Nesta", "Louis", "Lucie", "Marie", "Claude", "Marc", "Lourdingue", "Moi"]
-
+const nbOfQuestions = 20;
+const questions = ["Qui suis je ?", "Qui es tu ?"];
+const Answers = ["Nesta", "Louis", "Lucie", "Marie", "Claude", "Marc", "Lourdingue", "Moi"];
 
 function startQuizz() {
     document.getElementById("start").style.display = "none";
 
-    document.querySelector(".question").style.display ="block"
-    document.querySelector(".titleQuestion").innerText = questions[0];
+    document.getElementById("question").style.display ="block";
 
-    document.getElementById("labelAnswer1").innerText = Answers[0]
-    document.getElementById("labelAnswer2").innerText = Answers[1]
-    document.getElementById("labelAnswer3").innerText = Answers[2]
-    document.getElementById("labelAnswer4").innerText = Answers[3]
+    q = 1;
+
+    while (q < nbOfQuestions) {
+        NextQuestion(q)
+        q++;
+    }
+    
+}
+function NextQuestion(nbActualQuestion) {
+    console.log("next Question")
+
+    document.querySelector(".titleQuestion").innerText = questions[nbActualQuestion];
+
+    i = 0;
+    while (i < i+4) {
+        i++;
+        id = "labelAnswer" + String(i)
+        document.getElementById(id).innerText = Answers[i-1]
+    }
+    i += 4;
 }
